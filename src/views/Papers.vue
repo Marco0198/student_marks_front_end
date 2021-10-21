9d725035411931984e7477ef5622cce34eb121f7
Marco0198 <219049505@mycput.ac.za>
2021-10-21 09:41:56 +0200

    this is the last updat

--- src/views/Papers.vue
+++ src/views/Papers.vue
@@ -1,7 +1,7 @@
 <template>
 <Layout name="LayoutDefault">
     <div>
-     <!--  <h1 style="margin-bottom:50px;">Papers</h1><br>
+      <h1 style="margin-bottom:50px;">Papers</h1><br>
         <b-button style=" float:left; margin-bottom:20px;" class="btn  btn-warning" v-b-modal.modal-prevent-closing>Create paper</b-button>
 
         <div class="mt-3">
@@ -11,42 +11,38 @@
         <b-modal id="modal-prevent-closing" hide-footer ref="modal" title="Enter Your Details">
             <form ref="form" @submit="handleSubmit">
                 <b-form-group label="Paper Name" label-for="Paper-Number" invalid-feedback="Paper Number Name is required">
-                    <b-form-input v-model="paper-name" id="Paper-Number" required></b-form-input>
+                    <b-form-input v-model="paper_name" id="Paper-Number" required></b-form-input>
 
                 </b-form-group>
-                <b-form-group label="Student Number" label-for="student-number" invalid-feedback="student number is required">
-                    <b-form-input v-model="studentNumber" id="student-number" required></b-form-input>
+                <b-form-group label="paper time" label-for="paper_time" invalid-feedback="paper time is required">
+                    <b-form-input v-model="paper_time" id="paper_time" required></b-form-input>
 
                 </b-form-group>
-                <b-form-group label="Phone Number" label-for="Phone-number" invalid-feedback="phone number is required">
-                    <b-form-input v-model="studentPhoneNumber" id="Phone-number" required></b-form-input>
+                <b-form-group label="paper exam Id" label-for="paper_examId" invalid-feedback="paper_examId is required">
+                    <b-form-input v-model="paper_examId" id="paper_examId" required></b-form-input>
 
                 </b-form-group>
                 <b-form-group label="student Email" label-for="student-email" invalid-feedback="student email is required">
-                    <b-form-input type="email" v-model="studentEmail" id="name-input" required></b-form-input>
+                    <b-form-input type="email" v-model="paper_subject" id="name-input" required></b-form-input>
 
                 </b-form-group>
-                <b-form-group label="Student Password " label-for="Student-password" invalid-feedback="student Password  is required">
-                    <b-form-input type="password" v-model="studentPassword" id="Student-password" required></b-form-input>
+                <b-form-group label="paper_type" label-for="paper_type" invalid-feedback="paper_type  is required">
+                    <b-form-input  v-model="paper_type" id="paper_type" required></b-form-input>
 
                 </b-form-group>
-                <b-form-group label="Student address " label-for="Student-address" invalid-feedback="student address  is required">
-                    <b-form-input v-model="studentAddress" id="Student-address" required></b-form-input>
-
-                </b-form-group>
+              
                 <button style="margin-top:50px;" class="btn btn-primary">Register Student</button>
             </form>
         </b-modal>
         <table>
             <thead>
                 <tr>
-                    <th>Paper Number</th>
-                    <th>Student Name</th>
+                    <th>paper_Id</th>
+                    <th>paper_examId</th>
                     <th> Paper subject</th>
-                    <th> Marks decription</th>
-                   
-                    <th> marks</th>
-                    <th> Student number</th>
+                    <th> paper time</th>
+                    <th>paper name</th>
+                    <th> paper type</th>
                     <th> Action</th>
 
                 </tr>
@@ -67,34 +63,31 @@
         <b-modal id="my-modal" hide-footer title="Udpadte Your Details">
 
             <form ref="form" @submit="UpdateUser(user.studentId)">
-                <b-form-group label="Student Name" label-for="Student-Name-input" invalid-feedback="Student Name is required">
-                    <b-form-input v-model="studentName" id="Student-Name-input" required></b-form-input>
+               <b-form-group label="Paper Name" label-for="Paper-Number" invalid-feedback="Paper Number Name is required">
+                    <b-form-input v-model="paper_name" id="Paper-Number" required></b-form-input>
 
                 </b-form-group>
-                <b-form-group label="Student Number" label-for="student-number" invalid-feedback="student number is required">
-                    <b-form-input v-model="studentNumber" id="student-number" required></b-form-input>
+                <b-form-group label="paper time" label-for="paper_time" invalid-feedback="paper time is required">
+                    <b-form-input v-model="paper_time" id="paper_time" required></b-form-input>
 
                 </b-form-group>
-                <b-form-group label="Phone Number" label-for="Phone-number" invalid-feedback="phone number is required">
-                    <b-form-input v-model="studentPhoneNumber" id="Phone-number" required></b-form-input>
+                <b-form-group label="paper exam Id" label-for="paper_examId" invalid-feedback="paper_examId is required">
+                    <b-form-input v-model="paper_examId" id="paper_examId" required></b-form-input>
 
                 </b-form-group>
                 <b-form-group label="student Email" label-for="student-email" invalid-feedback="student email is required">
-                    <b-form-input type="email" v-model="studentEmail" id="name-input" required></b-form-input>
+                    <b-form-input type="email" v-model="paper_subject" id="name-input" required></b-form-input>
 
                 </b-form-group>
-                <b-form-group label="Student Password " label-for="Student-password" invalid-feedback="student Password  is required">
-                    <b-form-input v-model="studentPassword" id="Student-password" required></b-form-input>
+                <b-form-group label="paper_type" label-for="paper_type" invalid-feedback="paper_type  is required">
+                    <b-form-input  v-model="paper_type" id="paper_type" required></b-form-input>
 
                 </b-form-group>
-                <b-form-group label="Student address " label-for="Student-address" invalid-feedback="student address  is required">
-                    <b-form-input v-model="studentAddress" id="Student-address" required></b-form-input>
-
-                </b-form-group>
+              
                 <button style="margin-top:50px;" type="submit" class="btn btn-primary">update Student</button>
             </form>
         </b-modal>
--->
+
     </div>
 </Layout>
 </template>
@@ -154,12 +147,11 @@ export default {
 
             let formData = {
 
-                studentNumber: this.studentNumber,
-                studentName: this.studentName,
-                studentPhoneNumber: this.studentPhoneNumber,
-                studentEmail: this.studentEmail,
-                studentPassword: this.studentPassword,
-                studentAddress: this.studentAddress
+              paper_examId:this.paper_examId,
+              paper_subject:this.paper_subject,
+              paper_time:this.paper_time,
+              paper_name:this.paper_name,
+              paper_type:this.paper_type,
 
             }
             console.log(formData),
